17c77b99f2278189fbb96469dfb5fc2656ffca4f
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-07-21 03:23:56 +0800

    Create Archaeopteryx.ex

---
+++ natural/zoology/Archaeopteryx.ex
@@ -0,0 +1,41 @@
+An extinct genus of bird-like dinosaurs that lived around 150 million years ago during the Late Jurassic period.
+
+
+It represents an important evolutionary link between non-avian theropod dinosaurs and birds, although it is not the direct ancestor of modern birds. 
+
+
+The first complete fossil was found in Germany in 1861, and its features include a blend of reptilian and avian characteristics. 
+
+
+Archaeopteryx played a significant role in scientific debates about the origin and evolution of birds
+
+Kingdom:	Animalia
+
+Phylum:	Chordata
+
+Class:	Reptilia
+
+Clade:	Dinosauria
+
+Clade:	Saurischia
+
+Clade:	Theropoda
+
+Clade:	Paraves
+
+Family:	†Archaeopterygidae
+
+Genus:	†Archaeopteryx
+
+Meyer, 1861
+
+Over the years, fourteen body fossil specimens of Archaeopteryx have been found. 
+All of the fossils come from the limestone deposits, quarried for centuries, near Solnhofen, Germany. 
+These quarries excavate sediments from the Solnhofen Limestone formation and related units.[17][18] 
+The initial specimen was the first dinosaur to be discovered with feathers.
+Timeline of Archaeopteryx discoveries until 2007
+The initial discovery, a single feather, was unearthed in 1860 or 1861 and described in 1861 by Hermann von Meyer.[19] 
+It is now in the Natural History Museum of Berlin. Though it was the initial holotype, there were indications that it might not have been from the same animal as the body fossils.[11] 
+In 2019 it was reported that laser imaging had revealed the structure of the quill (which had not been visible since some time after the feather was described),
+and that the feather was inconsistent with the morphology of all other Archaeopteryx feathers known, leading to the conclusion that it originated from another dinosaur.[12]
+This conclusion was challenged in 2020 as being unlikely; the feather was identified on the basis of morphology as most likely having been an upper major primary covert feather.[13]
